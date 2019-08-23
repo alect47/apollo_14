c1ea49981e6e5eafae4f734092297c50b94c5cff
Alec Wells <alec.wells47@gmail.com>
2019-08-23 09:30:33 -0600

    Finish user story 1

---
+++ spec/features/austronauts/index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'When user visits astronauts index' do
+  it 'the see a list of astronauts with info' do
+    niel = Astronaut.create(name: 'Niel Armstrong', age: 37, job: "Commander")
+    phil = Astronaut.create(name: 'Phil Armstrong', age: 42, job: "Pilot")
+    visit '/astronauts'
+    expect(page).to have_content(niel.name)
+    expect(page).to have_content(niel.age)
+    expect(page).to have_content(niel.job)
+    expect(page).to have_content(phil.name)
+    expect(page).to have_content(phil.age)
+    expect(page).to have_content(phil.job)
+  end
+end
